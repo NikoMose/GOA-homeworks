18bdc304fdf5c7de8b852595c9354aeb22b9ab99
NikoMose <n74648439@gmail.com>
2025-03-27 20:03:37 +0400

    commit

--- day 06/homework/day06hw3.py
+++ day 06/homework/day06hw3.py
@@ -1,6 +1,6 @@
 year1 = 20
 year2 = 10
-print(year1 + year2)
-print(year1 // year2)
-print(year1 * year2)
-print(year1 - year2)
+print(year1 + year2) # plus
+print(year1 // year2) # divide
+print(year1 * year2) # multiply
+print(year1 - year2) # minus
